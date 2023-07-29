9910251cedd3ff0efddf03e7c6bcfc16af28d104
hunglq <104688414+1001VND@users.noreply.github.com>
2023-07-29 20:15:58 +0700

    section 5

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -12,22 +12,70 @@ namespace Mango.Services.AuthAPI.Service
         private readonly AppDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-        public AuthService(
+		public AuthService(
             AppDbContext db,
             UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager
-            )
+            RoleManager<IdentityRole> roleManager,
+			IJwtTokenGenerator jwtTokenGenerator
+
+			)
         {
             _db = db;
             _userManager = userManager;
             _roleManager = roleManager;
-        }
+			_jwtTokenGenerator = jwtTokenGenerator;
+		}
 
-        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+		public async Task<bool> AssignRole(string email, string roleName)
+		{
+			var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+			if (user != null)
+			{
+				if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+				{
+					//create role if it does not exist
+					_roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+				}
+				await _userManager.AddToRoleAsync(user, roleName);
+				return true;
+			}
+			return false;
+
+		}
+
+		public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
-            throw new NotImplementedException();
-        }
+			var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+
+			bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+			if (user == null || isValid == false)
+			{
+				return new LoginResponseDto() { User = null, Token = "" };
+			}
+
+			//if user was found , Generate JWT Token
+			//var roles = await _userManager.GetRolesAsync(user);
+			var token = _jwtTokenGenerator.GenerateToken(user);
+
+			UserDto userDTO = new()
+			{
+				Email = user.Email,
+				ID = user.Id,
+				Name = user.Name,
+				PhoneNumber = user.PhoneNumber
+			};
+
+			LoginResponseDto loginResponseDto = new LoginResponseDto()
+			{
+				User = userDTO,
+				Token = token
+			};
+
+			return loginResponseDto;
+		}
 
         public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
         {
