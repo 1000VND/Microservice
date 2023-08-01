71c4929b075e41ce8e3a15b610d202f56a1b0d4e
hunglq <104688414+1001VND@users.noreply.github.com>
2023-08-01 11:47:35 +0700

    section 6 Product API

---
+++ Mango.Web/Service/ProductService.cs
@@ -0,0 +1,72 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IBaseService _baseService;
+
+        public ProductService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> CreateProductsAsync(ProductDto ProductDto)
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = ProductDto,
+                Url = ProductAPIBase + "/api/Product"
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteProductsAsync(int id)
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.DELETE,
+                Url = ProductAPIBase + "/api/Product/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllProductsAsync()
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductAPIBase + "/api/Product"
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductAsync(string ProductCode)
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductAPIBase + "/api/Product/GetByCode/" + ProductCode
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductByIdAsync(int id)
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductAPIBase + "/api/Product/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateProductsAsync(ProductDto ProductDto)
+        {
+            return await _baseService.SeedAsync(new RequestDto()
+            {
+                ApiType = ApiType.PUT,
+                Data = ProductDto,
+                Url = ProductAPIBase + "/api/Product"
+            });
+        }
+    }
+}
